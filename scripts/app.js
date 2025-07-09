7929d3b055fe5ef16058667bdc64f72353be5b76
zahralmosawi <zalmosawii65@gmail.com>
2025-07-09 13:56:37 +0300

    add handle click

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,10 @@ function  init(){
     const  cells = []
     const gridWidth = 10
     const numberOfCells = gridWidth * gridWidth
+    const scoreElem = document.querySelector('#score')
+
     let duckPosition = 24;
+    let score = 0;
 
     function addDuck(){
         cells[duckPosition].classList.add('duck') //add duck class where the duck is(cell)
@@ -18,6 +21,14 @@ function  init(){
             addDuck()
         },3000)
     }
+    function handleClick(event){
+        //empty cell or cell with duck
+        if(event.target.classList.contains('duck')){
+            score += 1
+            scoreElem.textContent = `Score: ${score}`
+        }
+    }
+
     function createGrid(){
         //for every cell create a div and append them to our grid
         //loop dor creating 100 divs
@@ -25,6 +36,7 @@ function  init(){
             const cell = document.createElement('div')
             // cell.classList.add('duck') //add a duck class for each cell
             cell.textContent = i //fo develop just print out the number of each cell
+            cell.addEventListener('click', handleClick)
             cells.push(cell)
             gridElem.appendChild(cell)
         }
