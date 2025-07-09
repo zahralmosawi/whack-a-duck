f70fc8f14985f93272f15b30aa7b2c62e21bb610
zahralmosawi <zalmosawii65@gmail.com>
2025-07-09 14:49:38 +0300

    update it!!

--- scripts/app.js
+++ scripts/app.js
@@ -4,28 +4,45 @@ function  init(){
     const gridWidth = 10
     const numberOfCells = gridWidth * gridWidth
     const scoreElem = document.querySelector('#score')
+    const soundElem = document.querySelector('#sound')
+    const resultElem = document.querySelector('#result')
 
     let duckPosition = 24;
     let score = 0;
+    let totalDucks = 0;
 
     function addDuck(){
         cells[duckPosition].classList.add('duck') //add duck class where the duck is(cell)
+        totalDucks++;
     } 
     function removeDuck(){
         cells[duckPosition].classList.remove('duck')
     }
+    function endGame(){
+        // resultElem.textContent = `Game Over,  Your score is ${score}`
+        alert('Game Over,  Your score is ' +score)
+        score = 0
+        totalDucks = 0
+    }
     function play(){
         setInterval(()=>{
+            if(totalDucks < 10){
             removeDuck()
             duckPosition = Math.floor(Math.random() * numberOfCells)
-            addDuck()
+            addDuck()   
+            }else{
+                endGame()
+            }
         },3000)
     }
     function handleClick(event){
         //empty cell or cell with duck
         if(event.target.classList.contains('duck')){
+            soundElem.pause()
+            soundElem.currentTime = 0
             score += 1
             scoreElem.textContent = `Score: ${score}`
+            soundElem.play()
         }
     }
 
