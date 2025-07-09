e8b73ff6a6ec42a152b39454bf356d3c7dbb9279
zahralmosawi <zalmosawii65@gmail.com>
2025-07-09 11:40:43 +0300

    add duck class

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,19 @@
 function  init(){
     const gridElem = document.querySelector('.grid');
+    const  cells = []
+    const gridWidth = 10
+    const numberOfCells = gridWidth * gridWidth
     function createGrid(){
         //for every cell create a div and append them to our grid
         //loop dor creating 100 divs
-        for(let i =0; i < 100; i++){
+        for(let i =0; i < numberOfCells; i++){
             const cell = document.createElement('div')
+            cell.classList.add('duck') //add a duck class for each cell
             cell.textContent = i //fo develop just print out the number of each cell
+            cells.push(cell)
             gridElem.appendChild(cell)
         }
+        console.log(cells)
     }
     createGrid()
 }
